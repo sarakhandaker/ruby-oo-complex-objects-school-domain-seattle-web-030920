50e06a939a4b7c168cc02fd54c3d3060452bd6dc
github username <github email address>
2020-03-10 17:21:06 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    def initialize (name)
+        @name=name
+        @roster={}
+    end
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+        @roster[grade] ||=[]
+        @roster[grade]<< name
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        sorted = {}
+        @roster.each do |grade, students|
+          sorted[grade] = students.sort
+        end
+        sorted
+    end
+end
+
+school = School.new("Bayside High School")
